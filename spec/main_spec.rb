0b5505e952de7ee0624da3931e4584ec9c3722bb
zackp30 <zack.piper123@gmail.com>
2014-03-03 10:45:14 +0000

    Removed broken rspec method.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,11 +1,5 @@
 require './dby.rb'
 
-describe DBY::DBYConfig, '#initialize' do
-  it 'determines where the configuration file should go.' do
-    config = DBY::DBYConfig.new
-    config.initialize.should eq("#{Dir.home}/.dby.conf")
-  end
-end
 describe DBY::Package, '#install' do
   it 'is an un-implemented function, and is useless at this time' do
     package = DBY::Package.new
